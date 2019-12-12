92d229f79e7f9f160623dbee4088df126fb29156
olga_urazlova <olga_urazlova@epam.com>
2019-12-12 15:44:14 +0300

    Добавлены файлы проекта

---
+++ pages/login_page.py
@@ -0,0 +1,38 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+import time
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        substring = LoginPageLocators.SUBSTRING_LOGIN
+        assert substring in self.browser.current_url, "No 'login' in url on this page!"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "No login form on page!"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "No register form on page!"
+
+    def make_email_and_pass(self):
+        return (str(time.time()) + "@fakemail.org", "myStrongPassword№121")
+
+    def register_new_user(self, email, password):
+        self.email = email
+        self.password = password
+
+        email_input = self.browser.find_element(*LoginPageLocators.EMAIL_ADDR_INPUT)
+        pass_input = self.browser.find_element(*LoginPageLocators.PASSWORD1_INPUT)
+        pass_confirm = self.browser.find_element(*LoginPageLocators.PASSWORD2_INPUT)
+        reg_button = self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON)
+
+        email_input.send_keys(email)
+        pass_input.send_keys(password)
+        pass_confirm.send_keys(password)
+
+        reg_button.click()
---
+++ test_main_page.py
@@ -0,0 +1,33 @@
+from pages.main_page import MainPage
+from pages.locators import MainPageLocators
+from pages.login_page import LoginPage
+from pages.cart_page import CartPage
+import pytest
+
+@pytest.mark.login_guest
+class TestLoginFromMainPage(object):
+    def test_guest_should_see_login_link(self, browser):
+        link = MainPageLocators.MAIN_PAGE_LINK
+
+        page = MainPage(browser, link)
+
+        page.open()
+        page.should_be_login_link()
+
+    def test_guest_can_go_to_login_page(self, browser):
+        link = MainPageLocators.MAIN_PAGE_LINK
+
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
+
+def test_guest_cant_see_product_in_cart_opened_from_main_page(browser):
+    link = MainPageLocators.MAIN_PAGE_LINK
+
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    cart_page = CartPage(browser, browser.current_url)
+    cart_page.cart_should_be_empty()
